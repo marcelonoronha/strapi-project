c316652ad70c8ed7430e2c1ecc47d0fcc4f41c47
Marcelo Noronha <mbonoronha@gmail.com>
2021-06-29 11:37:45 -0300

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7d8fa189fce250ad75e67510454c4fb7'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd4b06811-99a6-4f84-b84f-cf7597fa8eb5'
+};
